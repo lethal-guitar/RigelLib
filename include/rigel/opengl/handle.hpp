64390003a9c60b057a7dbc203eec37196e35fc05
Nikolai Wuttke <lethal_guitar128@web.de>
2024-09-05 19:37:25 +0200

    Add RAII handle wrapper for common OpenGL object types

---
+++ include/rigel/opengl/handle.hpp
@@ -0,0 +1,183 @@
+/* Copyright (C) 2024, Nikolai Wuttke. All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <rigel/opengl/opengl.hpp>
+
+#include <type_traits>
+
+
+namespace rigel::opengl
+{
+
+namespace tag
+{
+
+struct Buffer
+{
+};
+
+struct Texture
+{
+};
+
+struct Shader
+{
+};
+
+struct Program
+{
+};
+
+} // namespace tag
+
+
+namespace detail
+{
+
+template <typename HandleType>
+GLuint createHandle()
+{
+  using namespace tag;
+
+  static_assert(
+    std::is_same_v<HandleType, Buffer> || std::is_same_v<HandleType, Texture> ||
+    std::is_same_v<HandleType, Program>);
+
+  GLuint handle = 0;
+
+  if constexpr (std::is_same_v<HandleType, Buffer>)
+  {
+    glGenBuffers(1, &handle);
+  }
+  else if constexpr (std::is_same_v<HandleType, Texture>)
+  {
+    glGenTextures(1, &handle);
+  }
+  else if constexpr (std::is_same_v<HandleType, Program>)
+  {
+    handle = glCreateProgram();
+  }
+
+  return handle;
+}
+
+
+inline GLuint createHandle(GLenum type)
+{
+  return glCreateShader(type);
+}
+
+
+template <typename HandleType>
+void destroyHandle(const GLuint handle)
+{
+  using namespace tag;
+
+  static_assert(
+    std::is_same_v<HandleType, Buffer> || std::is_same_v<HandleType, Texture> ||
+    std::is_same_v<HandleType, Shader> || std::is_same_v<HandleType, Program>);
+
+  if constexpr (std::is_same_v<HandleType, Buffer>)
+  {
+    glDeleteBuffers(1, &handle);
+  }
+  else if constexpr (std::is_same_v<HandleType, Texture>)
+  {
+    glDeleteTextures(1, &handle);
+  }
+  else if constexpr (std::is_same_v<HandleType, Shader>)
+  {
+    glDeleteShader(handle);
+  }
+  else if constexpr (std::is_same_v<HandleType, Program>)
+  {
+    glDeleteProgram(handle);
+  }
+}
+
+} // namespace detail
+
+
+template <typename HandleType>
+class HandleBase
+{
+public:
+  HandleBase() = default;
+
+  HandleBase(const HandleBase&) = delete;
+  HandleBase(HandleBase&& other)
+    : mHandle(other.mHandle)
+  {
+    other.mHandle = 0;
+  }
+
+  ~HandleBase()
+  {
+    if (mHandle == 0)
+    {
+      return;
+    }
+
+    detail::destroyHandle<HandleType>(mHandle);
+  }
+
+  HandleBase& operator=(const HandleBase&) = delete;
+  HandleBase& operator=(HandleBase&& other)
+  {
+    mHandle = std::exchange(other.mHandle, 0);
+    return *this;
+  }
+
+  operator GLuint() const { return mHandle; }
+
+  GLuint mHandle = 0;
+
+protected:
+  explicit HandleBase(GLuint handle)
+    : mHandle(handle)
+  {
+  }
+};
+
+
+template <typename HandleType>
+class Handle : public HandleBase<HandleType>
+{
+public:
+  using HandleBase<HandleType>::HandleBase;
+
+  static Handle<HandleType> create()
+  {
+    return Handle<HandleType>(detail::createHandle<HandleType>());
+  }
+};
+
+
+template <>
+class Handle<tag::Shader> : public HandleBase<tag::Shader>
+{
+public:
+  using HandleBase::HandleBase;
+
+  static Handle<tag::Shader> create(const GLenum type)
+  {
+    return Handle<tag::Shader>(detail::createHandle(type));
+  }
+};
+
+} // namespace rigel::opengl
